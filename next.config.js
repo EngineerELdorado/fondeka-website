541774d604b15791ce58ff22de0f75964c5be291
Denis Kalenga <deniskalenga94@gmail.com>
2026-01-07 14:31:13 +0100

    Dynamic linking helpers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [{ key: "Content-Type", value: "application/json" }]
+      },
+      {
+        source: "/.well-known/assetlinks.json",
+        headers: [{ key: "Content-Type", value: "application/json" }]
+      }
+    ];
+  }
 };
